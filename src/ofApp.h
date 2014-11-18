8d7cf2da0869e90bff4aa9a4c609c0bdd72dd298
yuky9797 <yukyaqua109@gmail.com>
2014-11-18 20:47:55 +0900

    first commit

---
+++ src/ofApp.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxiOS.h"
+#include "ofxiOSExtras.h"
+
+class ofApp : public ofxiOSApp {
+	
+    public:
+        void setup();
+        void update();
+        void draw();
+        void exit();
+	
+        void touchDown(ofTouchEventArgs & touch);
+        void touchMoved(ofTouchEventArgs & touch);
+        void touchUp(ofTouchEventArgs & touch);
+        void touchDoubleTap(ofTouchEventArgs & touch);
+        void touchCancelled(ofTouchEventArgs & touch);
+
+        void lostFocus();
+        void gotFocus();
+        void gotMemoryWarning();
+        void deviceOrientationChanged(int newOrientation);
+    
+    ofSoundPlayer soundone;
+    void audioin(float *input,int buffersize, int channels);
+    vector<float>left;
+    ofSoundStream soundstream;
+    int slide;
+    int elapsed;
+    //int red,blue,green;
+};
+
+
---
+++ src/ofApp.mm
@@ -0,0 +1,96 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofBackground(0, 0, 0);
+    ofEnableAlphaBlending();//透過度初期設定
+    soundone .loadSound("ray.mp3");
+    soundone.setLoop(true);
+    soundone.play();
+    
+    ofSetFrameRate(100);
+    elapsed =0;
+    slide=0;
+    int buffersize = 256;
+    left.assign(buffersize, 0.0);
+    soundstream.setup(this,0,2,44100,buffersize,4);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    elapsed++;
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetColor(20, 100, 155);//波形の色を指定
+    ofSetLineWidth(3);//線の幅を決定
+    slide++;//波形をずらすため一追加
+    if(slide ==130){slide=0;};//130ずれたらリセット
+    ofBeginShape();
+    for(unsigned int i =0; i<left.size(); i++){
+        ofVertex(i*50,500-left[i+slide]*3000.0f);
+    };
+    ofEndShape(false);//店集合の終了
+
+}
+
+//--------------------------------------------------------------
+void ofApp::exit(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchDown(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchMoved(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchUp(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchDoubleTap(ofTouchEventArgs & touch){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::touchCancelled(ofTouchEventArgs & touch){
+    
+}
+//--------------------------------------------------------------
+
+void ofApp::audioin(float *input, int buffersize, int channels){
+
+    if(elapsed%8==0){
+        for(int i=0; i<buffersize; i++){
+            left[i]    =input[i*2]*0.5;
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::lostFocus(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotFocus(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMemoryWarning(){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::deviceOrientationChanged(int newOrientation){
+
+}
